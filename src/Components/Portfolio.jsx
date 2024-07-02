fbac64f68b0d3ec99e381acf37f8bbf5da103a03
Anindita Priyadarshini <71321405+ANINDITAPRIYADARSHINI@users.noreply.github.com>
2024-07-02 18:34:08 +0000

    Portfolio updated

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/design-desk.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,35 +31,34 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Cosmic Explorer",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Unlock the Cosmos: Navigate a galaxy of stunning images with Cosmic Explorer, powered by NASA's Image Library API. Here, One can search Images and learn more about them.",
+    url: "cosmic-explorerr.netlify.app/",
   },
   {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    title: "Tenzies A Game",
+    description: "A fun dice game.",
+    url: "tenzies-a-game.netlify.app/",
   },
   {
-    title: "My Resume Site",
+    title: "Wild Paws",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Save the Wild Ones: The sole purpose of this site is to raise awareness towards protecting and taking care of the wild animals by adopting & taking the initiative towards this noble cause, by taking responsibility for those faunas.",
+    url: "wild-paws.netlify.app/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Gallery",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Explore Gallery Arena: Immerse in a diverse collection of projects. From a single-page website to a multipage website, it's a place of my creativity and innovation. Take a journey through my projects and discover the artistry within.",
+    url: "gallerry.netlify.app/",
   },
 ];
 
 const Portfolio = () => {
   return (
     <section className="padding" id="portfolio">
-      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+      <h2 style={{ textAlign: "center", color: "D8DEDC" }}>Portfolio</h2>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
