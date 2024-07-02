d1851a35351a73ab7f43539a14fb2a1e795cff64
Anindita Priyadarshini <71321405+ANINDITAPRIYADARSHINI@users.noreply.github.com>
2024-07-02 18:47:03 +0000

    Added a skills component

---
+++ src/Components/Skills.jsx
@@ -0,0 +1,77 @@
+/**
+ * Skills component
+ *
+ * Space to list my skills.
+ */
+
+import React from "react";
+
+/**
+ * Skills background image
+ *
+ * Below is a sample image. Upload the image of your choice into the "images"
+ * directory and import here for use. Then, set imageAltText to string that
+ * represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a image you
+ * freely use on your site.
+ */
+import image from "../images/motion-background.jpg";
+
+const imageAltText = "purple and blue abstract background";
+
+/**
+ * Sort description that expands on your title on the Home component.
+ */
+const description = "Here are the Technical skills I use the most :";
+
+/**
+ * List of some of skills or technologies you work on, are learning,
+ * passionate about, or enjoy,
+ */
+const skillsTool = ["HTML", "CSS", "JavaScript", "React.js", "Tailwind CSS", "Figma"];
+
+/**
+ * Use this to give more information about what you are passionate about,
+ * how you best work, or even a quote. This will help someone learn more
+ * about you on a professional level.
+ */
+const detailOrQuote = "Using these tools helps me to create innovative, unique user-experiences.";
+
+const Skills = () => {
+  return (
+    <section className="padding" id="about">
+      <img className="background" src={image} alt={imageAltText} />
+      <div
+        style={{
+          backgroundColor: "white",
+          width: "50%",
+          padding: "4rem",
+          margin: "3rem auto",
+          textAlign: "center",
+        }}
+      >
+        <h2>My SkillSet</h2>
+        <p className="large">{description}</p>
+        <hr />
+        <ul
+          style={{
+            textAlign: "left",
+            columns: 2,
+            fontSize: "1.25rem",
+            margin: "2rem 3rem",
+            gap: "3rem",
+          }}
+        >
+          {skillsTool.map((skill) => (
+            <li key={skill}>{skill}</li>
+          ))}
+        </ul>
+        <hr />
+        <p style={{ padding: "1rem 3rem 0" }}>{detailOrQuote}</p>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
