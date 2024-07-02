6f07dedc9abebe142572bd91b015862ad7f1aa75
Anindita Priyadarshini <71321405+ANINDITAPRIYADARSHINI@users.noreply.github.com>
2024-07-02 18:41:05 +0000

    about updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -18,25 +18,25 @@ import React from "react";
  */
 import image from "../images/motion-background.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "white and green abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a self-taught FrontEnd Developer with a master's degree in computer application(MCA). I enjoy creating unique and simplistic user interfaces in creative ways to solve user problems.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Web development",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Figma Wireframing",
+  "Problem Solver",
+  "Mobile-first UI",
+  "Responsive design",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my front-end experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
